235d041ce9d0fa0c99b7e0d5c9afa164e966b02b
unknown <prasad.chavala@vibhotech.com>
2025-01-23 16:08:08 +0530

    project-setup

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ const swaggerUi = require('swagger-ui-express');
 // const swaggerDocs = require('./Utils/swagger');
 const apiRouter = require('./Routes/routes'); // Assuming 'router.js' is in the 'routes' folder
 
-
 // Middleware
 app.use(bodyParser.json());
 // Serve Swagger UI
